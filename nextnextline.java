3889fcfad4edb4d1adf64972bfcde44759a240f8
vina786 <vinay76b@gmail.com>
2024-12-10 12:34:48 +0530

    Add files via upload

---
+++ nextnextline.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class nextnextline {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(a);
+        String b=sc.nextLine();
+        System.out.println(b);
+    }
+}
