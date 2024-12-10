85a372c80f69b1de9074a96a692490f481a51997
vina786 <vinay76b@gmail.com>
2024-12-10 12:35:14 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class reversestring {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String res=" ";
+        for(int i=0;i<a.length();i++)
+        res=a.charAt(i)+res;
+        System.out.println(res);
+    }
+}
