c612ce3f8d2ea355be274c79ccd9e6575c6c0992
vina786 <vinay76b@gmail.com>
2024-12-10 12:34:34 +0530

    Add files via upload

---
+++ foreachforloop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class foreachforloop {
+    public static void main(String args[])
+
+{
+Scanner sc=new Scanner(System.in);
+String a[]={"apple","orange","pineapple"};
+for(String b:a)
+System.out.print(b+",");
+}
+}
