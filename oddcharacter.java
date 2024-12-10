dd2d07a3a95d48c0623c640bc7f225607549bf9a
vina786 <vinay76b@gmail.com>
2024-12-10 12:35:01 +0530

    Add files via upload

---
+++ oddcharacter.java
@@ -0,0 +1,17 @@
+import java.util.*;;
+
+public class oddcharacter {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String ans=" ";
+        for(int i=1;i<a.length();i+=2)
+        {
+            ans+=a.charAt(i);
+        }
+        System.out.println(ans);
+        }
+    }
+
+
