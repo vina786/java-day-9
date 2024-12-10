dfcc82fdb003a39d428e5e99941fe8e6b9bfb76d
vina786 <vinay76b@gmail.com>
2024-12-10 12:34:05 +0530

    Add files via upload

---
+++ acceptingstring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class acceptingstring
+{
+    public static void main(String args[])
+    {
+        String a="hello";
+        String b=new String("hai");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
